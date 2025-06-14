3e017df3360dc948baf9c5fac3b53294756c2fe5
Eric Dubuisson <PSR09769@students.ephec.be>
2025-06-15 00:11:35 +0200

    feat: update GetAllPropertiesQuery to ignore global filters for admin and unauthenticated user access; add connection string to appsettings.Development.json

--- src/Application/Queries/Property/GetAllPropertiesQuery.cs
+++ src/Application/Queries/Property/GetAllPropertiesQuery.cs
@@ -50,7 +50,8 @@ namespace Application.Queries.Property
 
         public async Task<GetAllPropertiesResponse> Handle(GetAllPropertiesQuery request, CancellationToken cancellationToken)
         {
-            var query = _context.Properties.AsNoTracking();
+            // Ignore global filters so admin searches also see soft-deleted or unpublished items and for the unauthenticated user to see all properties
+            var query = _context.Properties.AsNoTracking().IgnoreQueryFilters();
 
             if (request.MinPrice.HasValue)
             {
