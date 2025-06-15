9d0a8beb1094195ac9f2c6e8b295da25e369b2be
Eric Dubuisson <PSR09769@students.ephec.be>
2025-06-15 20:15:10 +0200

    feat: add Type property to Property model for enhanced categorization and filtering capabilities

--- src/Infrastructure/Persistences/Migrations/20250615180014_UpdatePropertyModel.Designer.cs
+++ src/Infrastructure/Persistences/Migrations/20250615180014_UpdatePropertyModel.Designer.cs
@@ -4,6 +4,7 @@ using Infrastructure.Persistences;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Infrastructure.Persistences.Migrations
 {
     [DbContext(typeof(ImmotechDbContext))]
-    partial class ImmotechDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250615180014_UpdatePropertyModel")]
+    partial class UpdatePropertyModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
